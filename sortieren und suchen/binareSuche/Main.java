917d4e7a9cb60c3d845c302c9b138579655e8a11
Jakob <jakob-engel@mein.gmx>
2023-12-05 09:02:00 +0100

    binäre suche rukursiv und iterativ

--- sortieren und suchen/binareSuche/Main.java
+++ sortieren und suchen/binareSuche/Main.java
@@ -2,25 +2,48 @@ package binareSuche;
 
 public class Main {
   public static void main(String[] args) {
-    int[] zahlen = { 5, 7, 8, 15, 17, 18, 25, 100, 102, 145 };
-    System.out.println(zahlen.length);
-    System.out.println(suchebinaer(zahlen, 15, 0, zahlen.length));
-
+    int[] zahlen = new int[5000000];
+    for (int i = 0; i < zahlen.length; i++) {
+      zahlen[i] = i;
+      // System.out.println(zahlen[i]);
+    }
+    // System.out.println(suchebinaerRekursiv(zahlen, 47, 0, zahlen.length));
+    System.out.println(suchebinaerIterativ(zahlen, 0));
   }
 
-  public static int suchebinaer(int[] array, int gesucht, int untergrenze, int obergrenze) {
-    int arraylength = obergrenze - untergrenze;
-    if (array[arraylength / 2] > gesucht) {
-      System.out.println("kleiner");
-      return suchebinaer(array, gesucht, untergrenze, (int) (arraylength / 2));
-    } else if (array[arraylength / 2] < gesucht) {
-      System.out.println("groesser");
-      return suchebinaer(array, gesucht, (int) (arraylength / 2), obergrenze);
-    } else if ((array[arraylength / 2]) == gesucht) {
-      return arraylength / 2;
+  public static int suchebinaerIterativ(int[] array, int gesucht) {
+    int obergrenze = array.length;
+    int untergrenze = 0;
+    int zaehler = 0;
+    while (true) {
+      zaehler++;
+      int mitte = untergrenze + (obergrenze - untergrenze) / 2;
+      if (array[mitte] == gesucht) {
+        System.out.println("duchgaenge: " + zaehler);
+        return mitte;
+      } else if (obergrenze <= untergrenze) {
+        return -1;
+      } else if (array[mitte] < gesucht) {
+        untergrenze = mitte + 1;
+      } else if (array[mitte] > gesucht) {
+        obergrenze = mitte - 1;
+      }
     }
-    return -1;
+  }
 
+  public static int suchebinaerRekursiv(int[] array, int gesucht, int untergrenze, int obergrenze) {
+    int mitte = untergrenze + (obergrenze - untergrenze) / 2;
+    if (array[mitte] == gesucht) {
+      return mitte;
+    } else if (obergrenze <= untergrenze) {
+      return -1;
+    } else if (array[mitte] < gesucht) {
+      return suchebinaerRekursiv(array, gesucht, mitte + 1, obergrenze);
+    } else if (array[mitte] > gesucht) {
+      return suchebinaerRekursiv(array, gesucht, untergrenze, mitte - 1);
+    } else {
+      return -1;
+    }
   }
 
 }
