4ae8d6cec5df5515eeae6a8af3f811b5ab2e910b
Jakob <jakob-engel@mein.gmx>
2023-12-08 12:15:47 +0100

    irgendwasmit Strings

---
+++ sortieren und suchen/bubblesort/Kurs.java
@@ -0,0 +1,20 @@
+package bubblesort;
+
+import java.util.ArrayList;
+
+public class Kurs {
+  ArrayList<Schueler> schueler = new ArrayList<Schueler>();
+
+  public void schuelerHinzufuegen(Schueler schueler) {
+    this.schueler.add(schueler);
+  }
+
+  public int sucheseq(String nachname) {
+    for (int i = 0; i < schueler.size(); i++) {
+      if (schueler.get(i).getNachname().equals(nachname)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
---
+++ sortieren und suchen/bubblesort/Schueler.java
@@ -0,0 +1,19 @@
+package bubblesort;
+
+public class Schueler {
+  String nachname;
+  String vorname;
+
+  public Schueler(String nachname, String vorname) {
+    this.nachname = nachname;
+    this.vorname = vorname;
+  }
+
+  public String getNachname() {
+    return nachname;
+  }
+
+  public String getVorname() {
+    return vorname;
+  }
+}
